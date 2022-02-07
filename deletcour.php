8a7211094864aafa4e650f0c5ed7acf53a4850e5
soumiaessabiry <soumiaessabiry.se@gmail.com>
2022-02-07 16:29:55 +0100

    delet

--- deletcour.php
+++ deletcour.php
@@ -1,10 +1,11 @@
 <?php 
 include ('connexion.php');
-//delet data
+//delet data cours
 $cour=$_GET['id'];
 $query=" DELETE FROM  `courses` WHERE id='$cour'";
  mysqli_query($conn,$query);
 
 header('location:./indexcour.php');
+
 
 ?>
--- deletstuent.php
+++ deletstuent.php
@@ -1,6 +1,6 @@
 <?php 
 include ('connexion.php');
-//delet data
+//delet data student git
 $del=$_GET['id'];
 $query=" DELETE FROM `students` WHERE id='$del'";
  mysqli_query($conn,$query);
